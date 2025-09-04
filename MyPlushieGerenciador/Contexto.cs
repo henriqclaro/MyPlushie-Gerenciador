b482063eae00865e19d06664f6eb351a1b4c233e
henriqclaro <henrique.claro@unesp.br>
2025-09-04 11:20:20 -0300

    Add project files.

---
+++ MyPlushieGerenciador/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyPlushieGerenciador.Models;
+
+namespace MyPlushieGerenciador
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Compra> Compra { get; set; }
+        public DbSet<CompraProduto> CompraProduto { get; set; }
+    }
+}
---
+++ MyPlushieGerenciador/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyPlushieGerenciador")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyPlushieGerenciador")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("10b1e695-a2c4-4a73-b99b-5bc5bd924ecc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
